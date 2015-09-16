dfbfb5aa600aed275e29a7b20e32fc7bdaa6c456
BarnetteME1 <BarnetteME1@gmail.com>
2015-09-16 18:32:48 -0400

    word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,32 @@
+with open("sample.txt") as hounds_of_the_baskerville:
+    book = hounds_of_the_baskerville.read()
+
+from string import punctuation
+from string import whitespace
+from operator import itemgetter
+
+useless_words = ['a', 'able', 'about', 'across', 'after', 'all', 'almost', 'also', 'am',
+                 'among', 'an', 'and', 'any', 'are', 'as', 'at', 'be', 'because', 'been',
+                 'but', 'by', 'can', 'cannot','could','dear','did','do','does','either'
+                 ,'else','ever','every','for','from','get','got','had','has','have',
+                 'he','her','hers','him','his','how','however','i','if','in','into',
+                 'is','it','its','just','least','let','like','likely','may','me',
+                 'might','most','must','my','neither','no','nor','not','of','off',
+                 'often','on','only','or','other','our','own','rather','said',
+                 'say','says','she','should','since','so','some','than','that',
+                 'the','their','them','then','there','these','they','this','tis',
+                 'to','too','twas','us','wants','was','we','were','what','when',
+                 'where','which','while','who','whom','why','will','with','would',
+                 'yet','you','your']
+
+def words(book):
+    for char in book:
+        if char in punctuation:
+            book = book.replace(char, '')
+        if char in whitespace:
+            book = book.replace(char, ' ')
+    return book.lower().split()
+
 def word_frequency(book):
     book = words(book)
     word_count = {}
